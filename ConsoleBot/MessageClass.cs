68fb656b68144c34d80eb3384956f217699ac67d
Георгий <80705347+limeparrot@users.noreply.github.com>
2022-02-04 00:40:35 +0300

    Add files via upload

---
+++ ConsoleBot/MessageClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MessageClass
+{
+    public string userName { set; get; }
+    public string messageText { set; get; }
+    public string timeStamp { set; get; }
+
+    public override string ToString()
+    {
+        return $"{timeStamp} - {userName}: {messageText}";
+    }
+}
